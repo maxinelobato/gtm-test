105cabeace5c7733a50413b69c8fce20112ada45
maxinelobato <limabarreto.rg@gmail.com>
2025-07-23 17:44:29 -0300

    changes

--- src/app/components/Buttons/ButtonCta.tsx
+++ src/app/components/Buttons/ButtonCta.tsx
@@ -11,7 +11,7 @@ export function ButtonHome(props: ButtonHomeProps) {
     <div className="z-50 my-4 flex w-full justify-center sm:justify-start">
       <a
         aria-label="Contato Whatsapp"
-        className="focus:shadow-outline delay-0 inline-flex h-12 w-auto items-center justify-center rounded-full shadow-*-lg shadow-green-400/20 bg-brandCyan px-8 font-medium duration-75 ease-in-out hover:scale-105 focus:outline-none sm:h-14 md:w-auto"
+        className="focus:shadow-outline delay-0 inline-flex h-12 w-auto items-center justify-center rounded-full shadow-*-lg shadow-green-400/20 bg-green-500 px-8 font-medium duration-75 ease-in-out hover:scale-105 focus:outline-none sm:h-14 md:w-auto"
         {...props}
       />
     </div>
@@ -40,7 +40,7 @@ type ButtonFloatProps = ComponentProps<'a'>
 export function ButtonFloat(props: ButtonFloatProps) {
   return (
     <div className="fixed bottom-4 right-4 z-50 flex h-24 w-24 items-end justify-end p-2 hover:-translate-x-0 hover:scale-110 hover:transition-all hover:duration-300">
-      <div className="absolute flex items-center justify-center rounded-full shadow-*-lg shadow-gray-800/20 bg-green-400 p-4 hover:transition-all focus:outline-none">
+      <div className="absolute flex items-center justify-center rounded-full shadow-*-lg shadow-gray-800/20 bg-green-500 p-4 hover:transition-all focus:outline-none">
         <a aria-label="Contato Whatsapp" {...props} />
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         </h1>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <ButtonHome
-            href={`https://wa.me/5594981342011?text=Olá+vim+pelo+site+e+gostaria+de+mais+informações`}
+            href={`https://wa.me/5594984334432?text=Olá+vim+pelo+site+e+gostaria+de+mais+informações`}
           >
             <ButtonHomeText>Quero saber mais</ButtonHomeText>
             <ButtonHomeFlex>
@@ -27,51 +27,9 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <h1 className="text-white font-medium text-2xl sm:text-3xl">
+          GTM Test
+        </h1>
       </footer>
     </div>
   )
