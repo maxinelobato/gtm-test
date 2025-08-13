d91c4f25abd1a7d98e973bd13afded583784aada
maxinelobato <limabarreto.rg@gmail.com>
2025-08-13 20:26:10 -0300

    gtm brother

---
+++ src/app/(gtmbrother)/components/Buttons/ButtonCta.tsx
@@ -0,0 +1,48 @@
+import { ComponentProps } from 'react'
+
+export interface ButtonCta {
+  className: string
+}
+
+type ButtonHomeProps = ComponentProps<'a'>
+
+export function ButtonHome(props: ButtonHomeProps) {
+  return (
+    <div className="z-50 my-4 flex w-full justify-center sm:justify-start">
+      <a
+        aria-label="Contato Whatsapp"
+        className="focus:shadow-outline delay-0 inline-flex h-12 w-auto items-center justify-center rounded-full shadow-*-lg shadow-green-400/20 bg-green-500 px-8 font-medium duration-75 ease-in-out hover:scale-105 focus:outline-none sm:h-14 md:w-auto"
+        {...props}
+      />
+    </div>
+  )
+}
+
+type ButtonHomeFlexProps = ComponentProps<'div'>
+
+export function ButtonHomeFlex(props: ButtonHomeFlexProps) {
+  return <div className="z-50 ml-3 flex items-center" {...props} />
+}
+
+type ButtonHomeTextProps = ComponentProps<'div'>
+
+export function ButtonHomeText(props: ButtonHomeTextProps) {
+  return (
+    <div
+      className="z-50 text-center text-xs uppercase text-white sm:text-left sm:text-lg font-bold"
+      {...props}
+    />
+  )
+}
+
+type ButtonFloatProps = ComponentProps<'a'>
+
+export function ButtonFloat(props: ButtonFloatProps) {
+  return (
+    <div className="fixed bottom-4 right-4 z-50 flex h-24 w-24 items-end justify-end p-2 hover:-translate-x-0 hover:scale-110 hover:transition-all hover:duration-300">
+      <div className="absolute flex items-center justify-center rounded-full shadow-*-lg shadow-gray-800/20 bg-green-500 p-4 hover:transition-all focus:outline-none">
+        <a aria-label="Contato Whatsapp" {...props} />
+      </div>
+    </div>
+  )
+}
---
+++ src/app/(gtmbrother)/gtmbrother/page.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import {
+  ButtonHome,
+  ButtonHomeFlex,
+  ButtonHomeText,
+} from '../components/Buttons/ButtonCta'
+import { WhatsappLogoIcon } from '@phosphor-icons/react/dist/ssr'
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-white font-extrabold text-4xl sm:text-6xl">
+          GTM Brother
+        </h1>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <ButtonHome
+            href={`https://wa.me/5594984334432?text=Olá+vim+pelo+site+e+gostaria+de+mais+informações`}
+          >
+            <ButtonHomeText>Quero saber mais</ButtonHomeText>
+            <ButtonHomeFlex>
+              <WhatsappLogoIcon
+                className="h-5 w-5 sm:h-6 sm:w-6 fill-white drop-shadow-lg"
+                weight="fill"
+              />
+            </ButtonHomeFlex>
+          </ButtonHome>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <h1 className="text-white font-medium text-2xl sm:text-3xl">
+          GTM Brother
+        </h1>
+      </footer>
+    </div>
+  )
+}
