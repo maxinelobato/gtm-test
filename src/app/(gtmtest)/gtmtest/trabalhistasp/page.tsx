f1bc438e7ff46ae74d6f8c8089d294adf486bb19
maxinelobato <limabarreto.rg@gmail.com>
2025-08-26 00:46:14 -0300

    changes

--- src/app/(gtmtest)/gtmtest/trabalhistasp/page.tsx
+++ src/app/(gtmtest)/gtmtest/trabalhistasp/page.tsx
@@ -3,17 +3,16 @@ import {
   ButtonHome,
   ButtonHomeFlex,
   ButtonHomeText,
-} from '../components/Buttons/ButtonCta'
+} from '../../components/Buttons/ButtonCta'
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-white text-center font-extrabold text-4xl sm:text-6xl">
-          GTM Test/Trabalhista SP
+          GTM Test
         </h1>
-        <div className="flex gap-4 mx-auto items-center flex-col sm:flex-row">
+        <div className="flex mx-auto gap-4 items-center flex-col sm:flex-row">
           <ButtonHome
-            target="_blank"
             href={`https://wa.me/5594984334432?text=Olá+vim+pelo+site+e+gostaria+de+mais+informações`}
           >
             <ButtonHomeText>Quero saber mais</ButtonHomeText>
@@ -28,7 +27,7 @@ export default function Home() {
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <h1 className="text-white font-medium text-2xl sm:text-3xl">
-          GTM Test/Trabalhista SP
+          GTM Test
         </h1>
       </footer>
     </div>
